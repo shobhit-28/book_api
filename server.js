7eebd935a7680102c0c76e75ba9496f558f169f1
shobhit-28 <shobhitraj34@gmail.com>
2025-06-08 02:16:50 +0530

    removed console logs

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('Server starting')
-
 import authRoutes from './routes/authRoutes.js'
 import bookRoutes from './routes/bookRoutes.js'
 import reviewRoutes from './routes/reviewRoutes.js'
@@ -21,6 +19,5 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log('MongoDB connected');
-  app.listen(5000, () => console.log('Server running on port 5000'));
-}).catch((err) => console.log(err));
+  app.listen(5000);
+}).catch((err) => console.error(err));
