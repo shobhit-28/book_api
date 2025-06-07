b594bd9f8545bd3a10cb3898eca2a0c0d9c1b8d8
shobhit-28 <shobhitraj34@gmail.com>
2025-06-08 00:08:45 +0530

    added getBoks based on id of the book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 import Book from "../models/Book.js";
+import Review from "../models/Review.js";
 
 export const addBook = async (req, res) => {
     try {
@@ -67,6 +68,45 @@ export const getBooks = async (req, res) => {
                 books
             }
         });
+    } catch (error) {
+        res.status(500).json({ error: error?.message })
+    }
+}
+
+export const getBookById = async (req, res) => {
+    try {
+        const bookId = req.params.id;
+
+        const book = await Book.findById(bookId).lean();
+
+        if (!book) {
+            return res.status(404).json({ error: `Book not found` });
+        }
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        const reviews = await Review.find({ book: bookId }).populate('user', 'username').skip(skip).limit(limit).lean()
+
+        const totalReviews = await Review.countDocuments({ book: bookId })
+
+        const ratingStats = await Review.aggregate([
+            { $match: { book: book._id } },
+            { $group: { _id: `$book`, avgRating: { $avg: '$rating' } } }
+        ])
+
+        const avgRating = ratingStats[0]?.avgRating || 0;
+
+        res.status(200).json({
+            ...book,
+            averageRating: avgRating.toFixed(2),
+            totalReviews,
+            reviewsPage: page,
+            reviewsPerPage: limit,
+            reviews
+        })
+
     } catch (error) {
         res.status(500).json({ error: error?.message })
     }
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addBook, getBooks } from '../controllers/bookController.js';
+import { addBook, getBookById, getBooks } from '../controllers/bookController.js';
 import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/books', authenticateToken, addBook)
 router.get('/books', getBooks)
+router.get('/books/:id', getBookById)
 
 export default router;
